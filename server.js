e1438fe92250188767dd39a3cf476a007eed6e4f
Naseer Ahmed Khan <naseerkhan861@hotmail.com>
2017-06-28 15:32:18 +0500

    Add files to git repository

---
+++ server.js
@@ -0,0 +1,50 @@
+const express=require('express');
+const app=express();
+const fs=require('fs');
+const hbs=require('hbs');
+app.set('view engine','hbs');
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+app.use((req,res,next)=>{
+    var now=new Date().toString();
+    var log=`${now} ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log+'\n',(error)=>{
+        if(error){
+            console.log('Could Not write to file');
+        }
+    });
+    next();
+});
+app.use((req,res,next)=>{
+    res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname+"/public"));
+
+app.get('/',(req,res)=>{
+   res.render('home.hbs',{
+       pageTitle:'Home Page!!!',
+       welcomeMessage:'This is just a body Message and Nothing else'
+       })
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageTitle:'About Page!!!'
+    });
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:'Could Not handle Bad Stuffs'
+    });
+});
+
+app.listen(3000,()=>{
+    console.log('server started listening on PORT:3000');
+})
